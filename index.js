af20e36895a6a01e731cad1ef0db68d0247a85ce
AshtonJ7 <AshtonJ7@users.noreply.github.com>
2023-06-26 22:11:04 +0100

    questions for inquirer added

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 const inquirer = require('inquirer');
+const { MainMenuQuestions, AddDepartmentQuestions, AddRoleQuestions, AddEmployeeQuestions, UpdateEmployeeRoleQuestions } = require('questions.js')
+const EmployeeDatabase = require('/db/employeeDatabase')
